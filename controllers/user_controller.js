f655d8ebb99d2a27981082dfbb5b1fa3a62e2c2d
pratham mehtani <pratham@prathams-MacBook-Air.local>
2023-02-17 20:40:57 +0530

    sign in added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,8 +1,21 @@
 const User=require('../models/user');
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
+     //profile page is only visible when user is signed in
+     if(req.cookies.user_id)
+     {
+        User.findById(req.cookies.user_id,function(err,user)
+        {
+            if(user)
+            {
+                return res.render('user_profile',{
+                    title:"user's profile",
+                    user:user
+                })
+            }else res.redirect('/users/Sign-In')
+        })
+     }else{
+        return res.redirect('/users/Sign-In');
+     }
 }
  //render the sign in page
 module.exports.signIn=function(req,res)
@@ -50,5 +63,38 @@ module.exports.create=function(req,res)
 //sign in and create session for the user
 module.exports.createSession=function(req,res)
 {
-    
+    //steps to authenticate
+
+
+    //find the user
+    User.findOne({email:req.body.email},function(err,user)
+    {
+        if(err)
+        {
+            console.log("error in finding user signing in");
+            return;
+        }
+        //handle user found
+        if(user)
+        {
+             //handle mismatching of passwords
+            if(user.password!=req.body.password)
+            {
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+        }else{
+            //handle user not found
+            res.redirect('back');
+        }
+    })
+}
+
+module.exports.DeleteSession=function(req,res)
+{
+    // res.clearCookie("userid");
+    res.clearCookie('user_id');
+    return res.redirect('/users/profile')
 }
--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,6 @@ router.get('/profile',userController.profile)
 router.get('/Sign-In',userController.signIn);
 router.get('/Sign-Up',userController.signUp);
 router.post('/create',userController.create);
-
+router.post('/createSession',userController.createSession);
+router.get('/DeleteSession',userController.DeleteSession);
 module.exports=router;
