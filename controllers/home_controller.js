403cbc0b9d8dcd0b3bd6fa936d469b434a81e7d3
pratham mehtani <pratham@prathams-MacBook-Air.local>
2023-03-13 19:40:32 +0530

    async await method is added to simplify the code where there are more than 1 callback functions

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,28 +1,44 @@
-const Post=require('../models/post');
-const User=require('../models/user');
-module.exports.home=function (req,res) { 
-    // Post.find({}, function(err, posts){
-    //         return res.render('home', {
-    //             title: "Wohoo | Home",
-    //             posts:posts
-    //         });
-    //     });
-        //populate the user of every post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home', {
-                title: "Wohoo | Home",
-                posts:posts,
-                all_users:users
-            });
-        }) 
-    })
- }
+const Post = require('../models/post');
+const User = require('../models/user');
+// module.exports.home=function (req,res) { 
+//     //populate the user of every post
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//         path:'comments',
+//         populate:{
+//             path:'user'
+//         }
+//     })
+//     .exec(function(err,posts){
+//         User.find({},function(err,users){
+//             return res.render('home', {
+//                 title: "Wohoo | Home",
+//                 posts:posts,
+//                 all_users:users
+//             });
+//         }) 
+//     })
+//  }
+//async await method to make code cleaner
+module.exports.home = async function (req, res) {
+    try {
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            })
+        let users = await User.find({});
+        return res.render('home', {
+            title: "Wohoo | Home",
+            posts: posts,
+            all_users: users
+        })
+    } catch (err) {
+        console.log("error", err);
+        return;
+    }
+}
