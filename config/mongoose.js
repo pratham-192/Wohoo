762b31b88f44eb9f92eb36a1cf1e5ea9c39d4249
pratham mehtani <mehtani2020@gmail.com>
2023-07-05 17:59:35 +0530

    mongodb cluster added

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 const env=require('./environment');
-mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose.connect('mongodb+srv://mehtani2020:6ko7BtlgWjXg1M7J@cluster0.xa1l0vz.mongodb.net/');
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,"Error connecting to mongodb"));
 db.once('open',function () { 
