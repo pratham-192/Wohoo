d3e534d21611edf0f485b5ea98ca0631187ffd74
PRATHAM MEHTANI <mehtani2020@gmail.com>
2023-07-05 20:57:16 +0530

    port changed

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,73 +1,73 @@
-class ChatEngine {
-    constructor(chatBoxId, userEmail) {
-        this.chatBox = $(`#${chatBoxId}`);
-        this.userEmail = userEmail;
+// class ChatEngine {
+//     constructor(chatBoxId, userEmail) {
+//         this.chatBox = $(`#${chatBoxId}`);
+//         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:8000');
+//         this.socket = io.connect('http://localhost:8000');
 
-        if (this.userEmail) {
-            this.connectionHandler();
-        }
+//         if (this.userEmail) {
+//             this.connectionHandler();
+//         }
 
-    }
-
-
-    connectionHandler() {
-        let self = this;
-
-        this.socket.on('connect', function () {
-            console.log('connection established using sockets...!');
+//     }
 
 
-            self.socket.emit('join_room', {
-                user_email: self.userEmail,
-                chatroom: 'Wohoo'
-            });
+//     connectionHandler() {
+//         let self = this;
 
-            self.socket.on('user_joined', function (data) {
-                console.log('a user joined!', data);
-            })
+//         this.socket.on('connect', function () {
+//             console.log('connection established using sockets...!');
 
 
-        });
+//             self.socket.emit('join_room', {
+//                 user_email: self.userEmail,
+//                 chatroom: 'Wohoo'
+//             });
+
+//             self.socket.on('user_joined', function (data) {
+//                 console.log('a user joined!', data);
+//             })
 
 
-        $('#send-message').click(function () {
-            let msg = $('#chat-message-input').val();
-
-            if (msg != '') {
-                self.socket.emit('send_message', {
-                    message: msg,
-                    user_email: self.userEmail,
-                    chatroom: 'Wohoo'
-                });
-            }
-        });
+//         });
 
 
-        self.socket.on('receive_message', function (data) {
-            console.log('message received', data.message);
+//         $('#send-message').click(function () {
+//             let msg = $('#chat-message-input').val();
+
+//             if (msg != '') {
+//                 self.socket.emit('send_message', {
+//                     message: msg,
+//                     user_email: self.userEmail,
+//                     chatroom: 'Wohoo'
+//                 });
+//             }
+//         });
 
 
-            let newMessage = $('<li>');
+//         self.socket.on('receive_message', function (data) {
+//             console.log('message received', data.message);
 
-            let messageType = 'other-message';
 
-            if (data.user_email == self.userEmail) {
-                messageType = 'self-message';
-            }
+//             let newMessage = $('<li>');
 
-            newMessage.append($('<span>', {
-                'html': data.message
-            }));
+//             let messageType = 'other-message';
 
-            newMessage.append($('<sub>', {
-                'html': data.user_email
-            }));
+//             if (data.user_email == self.userEmail) {
+//                 messageType = 'self-message';
+//             }
 
-            newMessage.addClass(messageType);
+//             newMessage.append($('<span>', {
+//                 'html': data.message
+//             }));
 
-            $('#chat-messages-list').append(newMessage);
-        })
-    }
-}
+//             newMessage.append($('<sub>', {
+//                 'html': data.user_email
+//             }));
+
+//             newMessage.addClass(messageType);
+
+//             $('#chat-messages-list').append(newMessage);
+//         })
+//     }
+// }
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose=require('mongoose');
 // mongoose.connect('mongodb://localhost/development_db');
-mongoose.connect('mongodb+srv://mehtani2020:6ko7BtlgWjXg1M7J@cluster0.xa1l0vz.mongodb.net/');
+mongoose.connect('mongodb+srv://mehtani2020:6ko7BtlgWjXg1M7J@cluster0.xa1l0vz.mongodb.net/',{
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,"Error connecting to mongodb"));
 db.once('open',function () { 
